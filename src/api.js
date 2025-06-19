6b92a548e198501a1a50db7dd3ad3214d294cde8
pampaniyahardik <105900578+pampaniyahardik@users.noreply.github.com>
2025-06-19 19:20:20 +0530

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -2,8 +2,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://pampaniyahardik.pythonanywhere.com/api/',
+  baseURL: 'http://127.0.0.1:8000/api/',
   withCredentials: true,  // important for session!
 });
 
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 export default api;
