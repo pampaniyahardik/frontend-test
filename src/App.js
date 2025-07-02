b077fdd08b22d3564116ac55850fc6a961815cf0
Hardik <hardik58@hmail.com>
2025-07-02 12:58:49 +0530

    current affairs feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AuthPage from "./pages/AuthPage";
 import Dashboard from "./pages/Dashboard";
 import StartTest from "./components/StartTest";
 import { UserProvider } from "./context/UserContext";
+import CurrentAffairDetails from './components/CurrentAffairDetails';
 // import Counter from "./pages/Counter";
 
 function App() {
@@ -16,8 +17,8 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         {/* <Route path="/start-test" element={<StartTest />} /> */}
         <Route path="/start-test/:chapterId" element={<StartTest />} />
-
-
+         <Route path="/current-affairs/:date" element={<CurrentAffairDetails />} /> 
+            
       </Routes>
     </Router>
         </UserProvider>
